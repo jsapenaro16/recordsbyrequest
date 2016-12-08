137f65cfc4c5302bca17a019c82663a4ca5cd341
jsapenaro16 <jsapenaro16@northlandcaps.org>
2016-12-07 18:44:41 -0600

    Update MySQLManager.java

--- MySQLManager.java
+++ MySQLManager.java
@@ -16,22 +16,17 @@ import java.sql.*;
 
 public class MySQLManager {
 
-	private final int LOCAL_PORT = 3307;
-	private final int REMOTE_PORT = 3306;
-	private final String REMOTE_HOST = "66.6.115.212";
-
-	private final String REMOTE_USER = "pmrbr";
-	private final String REMOTE_PASS = "!tyler1";
-
 	private final String DB_USER = "pmrbr";
 	private final String DB_PASS = "!tyler1";
+	
+	private final String HOST = "localhost";
+	private final int PORT = 3306;
 
 	private final String DB_NAME = "records_by_request";
 	private final String DB_TABLE_NAME = "requests";
 
 	private final static Logger LOGGER = Logger.getLogger(MySQLManager.class.getName());
 
-	private Session session = null;
 
 	public static void main(String[] args) {
 		MySQLManager mng = new MySQLManager();
@@ -40,44 +35,14 @@ public class MySQLManager {
 	}
 
 	private void connect() {
-		int assigned_port;
-
-		JSch jsch = new JSch();
-		Session session;
-		try {
-			session = jsch.getSession(REMOTE_USER, REMOTE_HOST, 22);
-			session.setPassword(REMOTE_PASS);
-			// Additional SSH options. See your ssh_config manual for more
-			// options.
-			Properties config = new Properties();
-			config.put("StrictHostKeyChecking", "no");
-			config.put("Compression", "yes");
-			config.put("ConnectionAttempts", "2");
-			
-			session.setConfig(config);
-			session.connect();
-
-			assigned_port = session.setPortForwardingL(LOCAL_PORT, REMOTE_HOST, REMOTE_PORT);
-		} catch (JSchException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage());
-			return;
-		}
-
-		if (assigned_port == 0) {
-			LOGGER.log(Level.SEVERE, "Port forwarding failed!");
-			return;
-		}
-
 		// Build the database connection URL.
 		StringBuilder url = new StringBuilder("jdbc:mysql://localhost:");
-
-		// Use 'assigned_port' to establish database connection.
-		url.append(assigned_port).append("/").append(DB_NAME).append("?user=").append(DB_USER).append("&password=")
-				.append(DB_PASS);
-
+		url.append(PORT).append("/").append(DB_NAME);
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			Connection connection = DriverManager.getConnection(url.toString());
+			Connection connection = DriverManager.getConnection(url.toString(), DB_USER, DB_PASS);
+			System.out.println("Connected to database!");
 			DatabaseMetaData metadata = connection.getMetaData();
 			String[] tableType = { "TABLE", "VIEW" };
 			ResultSet tables = metadata.getTables(null, null, "%", tableType);
@@ -102,5 +67,6 @@ public class MySQLManager {
 		}
 
 	}
+	
 
 }
