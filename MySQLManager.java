2e39b5f699206a189828ff2e918c0562ad4081e1
jsapenaro16 <jsapenaro16@northlandcaps.org>
2016-12-13 01:21:22 -0600

    Update MySQLManager.java

--- MySQLManager.java
+++ MySQLManager.java
@@ -1,78 +1,53 @@
-
-/*
- * The purpose of the MySQLManager class is to establish a SSH connection to the remote development server
- * and to also establish a connection to the MySQL database via localhost.
- * 
- * JSch is a pure Java implementation of SSH2.
- * JSch allows you to connect to an sshd server and use port forwarding, X11 forwarding, file transfer, etc., 
- * and you can integrate its functionality into your own Java programs. JSch is licensed under BSD style license.
- */
-
 import java.sql.*;
 
 public class MySQLManager {
-
-	private final String DB_USER = "";
-	private final String DB_PASS = "";
-
-	private final String HOST = "136.63.192.54";
-	private final int PORT = 3306;
-
-	private final String DB_NAME = "records_by_request";
-	private final String DB_TABLE_NAME = "requests";
-
-	private Connection connection = null;
-	private DatabaseMetaData metadata;
-	public MySQLManager mng = null;
-	
-	
-	public MySQLManager(){
-		
-	}
-
-	public static void main(String[] args) {
-		MySQLManager mng = new MySQLManager();
-
-		mng.connect();
-	}
-
-	private void connect() {
-		// Build the database connection URL.
-		StringBuilder url = new StringBuilder("jdbc:mysql://136.63.192.54:");
-		url.append(PORT).append("/").append(DB_NAME);
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			Connection connection = DriverManager.getConnection(url.toString(), DB_USER, DB_PASS);
-			System.out.println("Connected to database!");
-			DatabaseMetaData metadata = connection.getMetaData();
-		} catch (IllegalAccessException | InstantiationException | ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	private String buildConnectionURL() {
-		StringBuilder url = new StringBuilder("jdbc:mysql://");
-		url.append(HOST + ':').append('/').append(DB_NAME);
-
-		return url.toString();
-	}
-	// InstantiationException
-	// ClassNotFoundException
-	// SQLException
-	/*
-	 * String[] tableType = { "TABLE", "VIEW" }; ResultSet tables =
-	 * metadata.getTables(null, null, "%", tableType); String tableName; while
-	 * (tables.next()) { tableName = tables.getString(3);
-	 * 
-	 * // Get the columns from this table ResultSet columns =
-	 * metadata.getColumns(null, tableName, null, null);
-	 * 
-	 * String columnName; int dataType; while (columns.next()) { columnName =
-	 * columns.getString(4); dataType = columns.getInt(5);
-	 * System.out.println(columnName); } } } catch (InstantiationException |
-	 * IllegalAccessException | ClassNotFoundException | SQLException e) { //
-	 * TODO Auto-generated catch block e.printStackTrace(); }
-	 */
-
+    
+    
+    private static final String DB_USER = "";
+    private static final String DB_PASS = "";
+    
+    private static final String HOST = ""; //136.63.192.54
+    private static final int PORT = 3306;
+    
+    protected static final String DB_NAME = "records_by_request";
+    protected static final String DB_TABLE_NAME = "requests";
+    
+    
+    private static Connection connection = null;
+    
+    protected static DatabaseMetaData metaData = null;
+    
+    private static GUI db_interface = null;
+    
+    
+    public static void main(String[] args){
+        connect(DB_USER, DB_PASS);
+        Database database = new Database();
+        GUI.createNewGUI();
+    }
+    
+    private static void connect(String db_user, String db_pass){
+        String connectionURL = buildConnectionURL(HOST, PORT, DB_NAME);
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(connectionURL, DB_USER, DB_PASS);
+            metaData = connection.getMetaData();
+            
+            System.out.println("Connected to database!");
+            System.out.println("______________________");
+            
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+    }
+    
+    }
+    
+     private static String buildConnectionURL(String host, int port, String db_name){
+        StringBuilder URL = new StringBuilder("jdbc:mysql://");
+        URL.append(host + ':').append(port).append('/').append(db_name);
+        
+        return URL.toString();
+    } 
+    
 }
