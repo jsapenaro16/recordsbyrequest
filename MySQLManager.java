3be22f551d4ed2f1bd71d005e9d1378f41d79bd1
jsapenaro16 <jsapenaro16@northlandcaps.org>
2016-12-15 02:10:26 -0600

    Update MySQLManager.java

--- MySQLManager.java
+++ MySQLManager.java
@@ -1,4 +1,6 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MySQLManager {
 
@@ -8,41 +10,38 @@ public class MySQLManager {
 	private static final String HOST = ""; // 136.63.192.54
 	private static final int PORT = 3306;
 
-	protected static final String DB_NAME = "records_by_request";
-	protected static final String DB_TABLE_NAME = "requests";
+	private static final String DB_NAME = "records_by_request";
+	private static final String DB_TABLE_NAME = "requests";
 
 	protected static Connection connection = null;
 
-	protected static DatabaseMetaData metaData = null;
-
+	public static MainGUI mainGUI = null;
 	public static Database database = null;
-	
-	
+
 	public static void main(String[] args) {
-		connect(DB_USER, DB_PASS);
-		database = new Database();
+		connect();
+		database = new Database(
+				new String[] { "Project", "Status", "Date Required", "Time Required", "Date Submitted" });
+		mainGUI = new MainGUI();
 		database.executeQuery("SELECT * FROM " + DB_TABLE_NAME);
-		GUI.createNewGUI();
 	}
 
-	private static void connect(String db_user, String db_pass) {
+	private static void connect() {
 		String connectionURL = buildConnectionURL(HOST, PORT, DB_NAME);
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(connectionURL, DB_USER,
-					DB_PASS);
-			metaData = connection.getMetaData();
 
-			System.out.printf("Connected to %s:%d:%s %n", HOST, PORT, DB_NAME);
+			connection = DriverManager.getConnection(connectionURL, DB_USER, DB_PASS);
+
+			System.out.printf("Connected to %s:%d:%s%n", HOST, PORT, DB_NAME);
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 
 	}
 
-	private static String buildConnectionURL(String host, int port,
-			String db_name) {
+	private static String buildConnectionURL(String host, int port, String db_name) {
 		StringBuilder URL = new StringBuilder("jdbc:mysql://");
 		URL.append(host + ':').append(port).append('/').append(db_name);
 
