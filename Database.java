8ca10f8dca595b2a630e2fd0f668ed129fd855cc
jsapenaro16 <jsapenaro16@northlandcaps.org>
2017-01-04 01:15:38 -0600

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,6 +1,7 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Vector;
 
@@ -11,6 +12,7 @@ public class Database extends MySQLManager {
 	private ResultSet resultSet = null;
 
 	protected Vector<String> visibleColumnNames = new Vector<String>();
+	protected ArrayList<String> columnNames = getColumnNames();
 
 	public Database(String[] visibleColumnNames) {
 		Collections.addAll(this.visibleColumnNames, visibleColumnNames);
@@ -33,8 +35,51 @@ public class Database extends MySQLManager {
 				String timeRequired = resultSet.getString(3);
 				String dateSubmitted = resultSet.getString(4);
 				String requestID = resultSet.getString(1);
+				String patientFirstName = resultSet.getString(12);
+				String patientLastName = resultSet.getString(13);
 
-				Object[] rowData = { project, status, dateRequired, timeRequired, dateSubmitted, requestID };
+				Object[] rowData = { project, status, dateRequired, timeRequired, dateSubmitted, requestID, patientFirstName, patientLastName};
+
+				mainGUI.tableModel.addRow(rowData);
+			}
+			resultSet.close();
+			statement.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	protected void executeSearchQuery(String keyword) {
+
+		StringBuilder searchQuery = new StringBuilder("SELECT * FROM ").append(DB_TABLE_NAME).append(" WHERE ");
+		for (int i = 0; i < columnNames.size(); i++) {
+			searchQuery.append(columnNames.get(i) + " LIKE '%" + keyword + "%'");
+
+			if (i != (columnNames.size() - 1)) {
+				searchQuery.append(" OR ");
+			}
+		}
+
+		try {
+			System.out.println(searchQuery.toString());
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(searchQuery.toString());
+
+			mainGUI.tableModel.setRowCount(0);
+
+			while (resultSet.next()) {
+				String project = resultSet.getString(5);
+				String status = resultSet.getString(6);
+				String dateRequired = resultSet.getString(2);
+				String timeRequired = resultSet.getString(3);
+				String dateSubmitted = resultSet.getString(4);
+				String requestID = resultSet.getString(1);
+				String patientFirstName = resultSet.getString(12);
+				String patientLastName = resultSet.getString(13);
+
+				Object[] rowData = { project, status, dateRequired, timeRequired, dateSubmitted, requestID, patientFirstName, patientLastName};
 
 				mainGUI.tableModel.addRow(rowData);
 			}
@@ -54,5 +99,23 @@ public class Database extends MySQLManager {
 		} else {
 			System.out.println("Current query has a value of: null.");
 		}
+	}
+
+	private ArrayList<String> getColumnNames() {
+
+		ArrayList<String> columnNames = new ArrayList<String>();
+
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("DESCRIBE " + DB_TABLE_NAME);
+
+			while (resultSet.next()) {
+				columnNames.add(resultSet.getString(1));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return columnNames;
 	}
 }
