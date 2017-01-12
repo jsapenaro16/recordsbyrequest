49123b69986c04eed940cc27a6f1d712cc44e9b7
jsapenaro16 <jsapenaro16@northlandcaps.org>
2017-01-11 23:22:36 -0600

    Update MySQLManager.java

--- MySQLManager.java
+++ MySQLManager.java
@@ -7,10 +7,10 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class MySQLManager {
 
-	private static final String DB_USER = "pmrbr";
-	private static final String DB_PASS = "!tyler1";
+	private static final String DB_USER = "";
+	private static final String DB_PASS = "";
 
-	private static final String HOST = "136.63.192.54"; // 136.63.192.54
+	private static final String HOST = "localhost"; // 136.63.192.54
 	private static final int PORT = 3306;
 
 	private static final String DB_NAME = "records_by_request";
