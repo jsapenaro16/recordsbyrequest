dd6b3864b9a8b0907a12b257434a078b2402102c
jsapenaro16 <jsapenaro16@northlandcaps.org>
2017-01-11 23:42:07 -0600

    Update Row.java

--- Row.java
+++ Row.java
@@ -1,29 +1,33 @@
 //public class Row implements Comparable<Row> {
 public class Row {
 
-	protected String[] information = new String[18];
-	protected String[] visibleRowData = new String[10];
+    protected String[] information = new String[18];
+    protected String[] visibleRowData = new String[10];
 
-	public Row(String[] information) {
-		this.information = information;
+    protected String status = null;
 
-		visibleRowData[0] = information[4];
-		visibleRowData[1] = information[5];
-		visibleRowData[2] = information[1];
-		visibleRowData[3] = information[2];
-		visibleRowData[4] = information[3];
-		visibleRowData[5] = information[0];
-		visibleRowData[6] = information[7];
-		visibleRowData[7] = information[8];
-		visibleRowData[8] = information[11];
-		visibleRowData[9] = information[12];
+    public Row(String[] information) {
+        this.information = information;
 
-	}
+        visibleRowData[0] = information[4];
+        visibleRowData[1] = information[5];
+        visibleRowData[2] = information[1];
+        visibleRowData[3] = information[2];
+        visibleRowData[4] = information[3];
+        visibleRowData[5] = information[0];
+        visibleRowData[6] = information[7];
+        visibleRowData[7] = information[8];
+        visibleRowData[8] = information[11];
+        visibleRowData[9] = information[12];
 
-	public void printRows() {
-		for (String data : visibleRowData) {
-			System.out.println(data);
-		}
-	}
+        status = information[5];
+
+    }
+
+    public void printRows() {
+        for (String data : visibleRowData) {
+            System.out.println(data);
+        }
+    }
 
 }
