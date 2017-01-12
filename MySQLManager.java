6b1b1df4c57ec68ac10ec3a39ee174d48f2fda79
jsapenaro16 <jsapenaro16@northlandcaps.org>
2017-01-11 23:20:22 -0600

    Update MySQLManager.java

--- MySQLManager.java
+++ MySQLManager.java
@@ -7,10 +7,10 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class MySQLManager {
 
-	private static final String DB_USER = "";
-	private static final String DB_PASS = "";
+	private static final String DB_USER = "pmrbr";
+	private static final String DB_PASS = "!tyler1";
 
-	private static final String HOST = "localhost"; // 136.63.192.54
+	private static final String HOST = "136.63.192.54"; // 136.63.192.54
 	private static final int PORT = 3306;
 
 	private static final String DB_NAME = "records_by_request";
@@ -24,8 +24,10 @@ public class MySQLManager {
 	public static void main(String[] args) {
 		setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
 		connect();
-		database = new Database(new String[] { "Project", "Status", "Date Required", "Time Required", "Date Submitted",
-				"Request ID", "Requester First", "Requester Last", "Patient First", "Patient Last" });
+		database = new Database(new String[] { "Project", "Status",
+				"Date Required", "Time Required", "Date Submitted",
+				"Request ID", "Requester First", "Requester Last",
+				"Patient First", "Patient Last" });
 		mainGUI = new MainGUI();
 		database.executeSearchQuery("");
 	}
@@ -36,7 +38,8 @@ public class MySQLManager {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
-			connection = DriverManager.getConnection(connectionURL, DB_USER, DB_PASS);
+			connection = DriverManager.getConnection(connectionURL, DB_USER,
+					DB_PASS);
 
 			System.out.printf("Connected to %s:%d:%s%n", HOST, PORT, DB_NAME);
 		} catch (ClassNotFoundException | SQLException e) {
@@ -45,7 +48,8 @@ public class MySQLManager {
 
 	}
 
-	private static String buildConnectionURL(String host, int port, String db_name) {
+	private static String buildConnectionURL(String host, int port,
+			String db_name) {
 		StringBuilder URL = new StringBuilder("jdbc:mysql://");
 		URL.append(host + ':').append(port).append('/').append(db_name);
 
@@ -55,8 +59,8 @@ public class MySQLManager {
 	private static void setLookAndFeel(String lookAndFeel) {
 		try {
 			UIManager.setLookAndFeel(lookAndFeel);
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e) {
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
 			e.printStackTrace();
 		}
 	}
